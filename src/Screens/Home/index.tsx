92bdd3fd00c7878f5745bae3d424c4cfa443eafc
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-09-06 18:16:35 -0300

    Componente Header

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { StatusBar } from 'expo-status-bar';
 
-import { Container, Title } from './styles';
+import { Container, Header, HeaderContent, TotalCars } from './styles';
+
+import Logo from '../../assets/logo.svg';
 
 export function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar style="light" translucent backgroundColor="transparent" />
+
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   );
 }
